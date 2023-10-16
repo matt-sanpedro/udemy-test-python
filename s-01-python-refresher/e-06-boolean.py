1f995f0fef2b4601f54bb4cf3e35da794982f895
matt-sanpedro <m.sanpedro@tcs.com>
2023-10-16 12:14:16 -0700

    Added intro to boolean and behavior of is keyword

---
+++ s-01-python-refresher/e-06-boolean.py
@@ -0,0 +1,7 @@
+friends = ["Ana", "Alexandra"]
+abroad = ["Ana", "Alexandra"]
+
+print(friends == abroad) # True
+
+# outputs False because a new list is created in memory
+print(friends is abroad) # False

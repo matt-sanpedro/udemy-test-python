1e93731c66cd61838ae46a601766d84595e0ac2a
matt-sanpedro <m.sanpedro@tcs.com>
2023-10-26 19:17:34 -0700

    Added test for ask_create_blog and test_user_input

--- s-02-automated-software-test/blog/tests/system/test_app.py
+++ s-02-automated-software-test/blog/tests/system/test_app.py
@@ -5,6 +5,18 @@ import app
 from blog import Blog
 
 class AppTest(TestCase):
+    
+    def test_menu_prints_prompt(self):
+        with patch("builtins.input", return_value = "q") as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+            
+    def test_menu_calls_print_blogs(self):
+        with patch("app.print_blogs") as mocked_print_blogs:
+            with patch("builtins.input", return_value = "q"):
+                app.menu()
+                mocked_print_blogs.assert_called()
+            
     def test_print_blogs(self):
         # test printing to console by patching the print statement
         # create the blog instant to test against
@@ -20,7 +32,15 @@ class AppTest(TestCase):
             # print("type: ", type(app.print_blogs()))
             mocked_print.assert_called_with("- Test by Test Author (0 posts)")
 
-    def test_menu_prints_prompt(self):
+    def test_user_input(self):
+        # test the input q to ensure user can exit
+        with patch("builtins.input"):
+            expected = app.get_user_input().return_value = "q"
+            self.assertEqual(expected, "q")
+            
+    def test_ask_create_blog(self):
         with patch("builtins.input") as mocked_input:
-            app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+            # in patching, instead of returning a single value, can call side_effect
+            mocked_input.side_effect = ("Test", "Test Author")
+            app.ask_create_blog()
+            self.assertIsNotNone(app.blogs.get("Test"))
